2a7d6ff1be2b7a36c8204be04b28329c05cf9226
John Trocchi <jtrocchi1@gmail.com>
2020-09-10 14:39:01 -0400

    add menu- no styles

--- pages/menu.vue
+++ pages/menu.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
-    Menu Page
+    <h2>Menu</h2>
+    <h3>Hot Subs</h3>
+    <p>Plain S - $6.65 L - $7.10</p>
+    <p>Cheese S - $7.15 L - $7.50</p>
+    <p>Pepper S - $7.15 L - $7.50</p>
+    <p>Mushroom S - $7.15 L - $7.50</p>
+    <p>Onion S - $7.15 L - $7.50</p>
+    <p>Steak S - $7.15 L - $7.50</p>
+    <p>2-Way S - $7.35 L - $7.69</p>
+    <p>3-Way S - $7.45 L - $7.99</p>
+    <p>Steak Bomb S - $7.75 L - $8.25</p>
+    <p>Hamburger S - $5.65 L - $6.15</p>
+    <p>Cheese Burger S - $6.75 L - $7.25</p>
+    <p>Hot Pastrami S - $7.75 L - $8.25</p>
+    <p>Chicken Cutlet S - $7.95 L - $8.25</p>
+    <p>Eggplant S - $7.15 L - $8.25</p>
+    <p>Pepper & Egg S - $7.15 L - $7.50</p>
+    <p>Chicken & Broccoli S - $7.15 L - $7.50</p>
+    <p>Meatball S - $7.15 L - $7.50</p>
+    <p>Hot Veggie S - $7.15 L - $7.50</p>
+    <h3>Cold Subs</h3>
+    <p>Italian S - $7.15 L - $7.45</p>
+    <p>American S - $7.15 L - $7.45</p>
+    <p>Ham & Cheese S - $7.15 L - $7.45</p>
+    <p>Tuna S - $7.15 L - $7.50</p>
+    <p>BLT Sub S - $7.50</p>
+    <p>Roast Beef S - $7.15 L - $7.45</p>
+    <p>Turkey S - $7.15 L - $7.45</p>
+    <p>Chicken Salad S - $7.15 L - $7.45</p>
+    <p>Cold Veggie S - $7.15 L - $7.45</p>
+    <p>North End S - $8.50</p>
+    <h3>Calzones</h3>
+    <p>Meat $6.99</p>
+    <p>Spinach $6.99</p>
+    <h3>Salads</h3>
+    <p>Garden $5.25</p>
+    <p>Greek $6.95</p>
+    <p>Antipasto $6.95</p>
+    <p>Yuma $7.50</p>
+    <p>Grilled Chicken $7.50</p>
+    <p>Tuna In Oil $7.50</p>
+    <p>Extra Dressing or Sauce $.75</p>
+    <h3>Sides</h3>
+    <p>Lasagna $7.45</p>
+    <p>Stuffed Shells $6.99</p>
+    <p>Manicotti $6.99</p>
+    <h3>Extras</h3>
+    <p>Meatballs $1.75 Each</p>
+    <p>Extra Cheese $0.50</p>
+    <p>Bacon $1.75</p>
   </div>
 </template>
 
